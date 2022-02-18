863c7ee0438c9a62c7c1b962d7214de8263570d2
ftrlola16 <39681018+ftrlola16@users.noreply.github.com>
2022-02-18 10:13:22 +0100

    add all my files

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-function myFunction() {
-  var x = document.getElementById("navbar2");
-  if (x.className === "navbar-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar-nav";
-  }
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const mobileMenu = document.getElementById('mobile-menu');
+const allLinks = document.querySelectorAll('.mobile-links')
+
+
+const openMobileMenu = () => {
+  mobileMenu.style.top = '0';
+  mobileMenu.style.transition = '500ms ease-in-out'
 }
+
+const closeMobileMenu = () => {
+  mobileMenu.style.top = '-110%';
+}
+
+hamburger.addEventListener('click', openMobileMenu);
+close.addEventListener('click', closeMobileMenu)
+
+allLinks.forEach(link => {
+  link.addEventListener('click', closeMobileMenu)
+})
 
 
 const goUp = document.querySelector('.go-up');
+const newHeader = document.getElementsByTagName('header');
+
 window.onscroll = () => {
   if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
     newHeader.classList.add('nav-colored');
